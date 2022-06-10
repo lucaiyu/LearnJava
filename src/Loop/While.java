45ce908ad2f78d518b1c3dd78d902d2e87fb8c7a
lucaiyu <aiyu2009@126.com>
2022-06-10 14:38:07 +0800

    Add files via upload

---
+++ src/Loop/While.java
@@ -0,0 +1,11 @@
+package Loop;
+
+public class While {
+    public static void main(String[] args) {
+        int counter = 0;
+        while (counter < 10){
+            System.out.println("Counter is "+ counter);
+            counter++;
+        }
+    }
+}
