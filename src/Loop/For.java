c81eba819d3e0754461b28b8577b9a41c3b6fa1a
lucaiyu <aiyu2009@126.com>
2022-06-13 16:09:34 +0800

    Add files via upload

---
+++ src/Loop/For.java
@@ -0,0 +1,9 @@
+package Loop;
+
+public class For {
+    public static void main(String[] args) {
+        for (int i = 0 ; i < 10 ; i++){
+            System.out.println("I is "+i);
+        }
+    }
+}
