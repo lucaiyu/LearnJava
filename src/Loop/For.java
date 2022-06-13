03c11f70c8ddb65560ab80318e03c049c9f7111a
lucaiyu <aiyu2009@126.com>
2022-06-13 16:08:56 +0800

    Delete src directory

--- src/Loop/For.java
+++
@@ -1,9 +0,0 @@
-package Loop;
-
-public class For {
-    public static void main(String[] args) {
-        for (int i = 0 ; i < 10 ; i++){
-            System.out.println("I is "+i);
-        }
-    }
-}
